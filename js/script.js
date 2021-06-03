162bd731a50feadd3c35614a4e71b8e6f1b16d24
Gianluca <gl.galassini@gmail.com>
2021-06-03 15:46:52 +0200

    versione con indice 1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 var listona = [];
 
-for (var i = 0; i <= 100; i++) {
-    if (i >= 1 && i % 3 == 0 && i % 5 == 0) {
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
         listona.push(i + " FizzBuzz");
-    } else if (i >= 1 && i % 3 == 0) {
+    } else if (i % 3 == 0) {
         listona.push(i + " Fizz");
-    } else if (i >= 1 && i % 5 == 0) {
+    } else if (i % 5 == 0) {
         listona.push(i + " Buzz");
     } else {
         listona.push(i);
