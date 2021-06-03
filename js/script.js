d9df1eb6b3b9112ed50cb0d05720c95b4126484c
Gianluca <gl.galassini@gmail.com>
2021-06-03 15:43:32 +0200

    fine esercizio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+listona = [];
+
+for (var i = 0; i <= 100; i++) {
+    if (i >= 1 && i % 3 == 0 && i % 5 == 0) {
+        listona.push(i + " FizzBuzz");
+    } else if (i >= 1 && i % 3 == 0) {
+        listona.push(i + " Fizz");
+    } else if (i >= 1 && i % 5 == 0) {
+        listona.push(i + " Buzz");
+    } else {
+        listona.push(i);
+    };
+}
+
+document.getElementById("printer").innerHTML = listona;
